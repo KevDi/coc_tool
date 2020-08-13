7f70dc575582d9bfb33354c1abf42a768f249d8f
KevDi <kevin.dick91@yahoo.de>
2020-08-13 12:50:49 +0200

    First Steps to display War Infos

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=coc_tool.py
+FLASK_ENV=development
--- app/wars/routes.py
+++ app/wars/routes.py
@@ -9,3 +9,10 @@ from app.models import War
 def wars():
     wars = War.query.all()
     return render_template("wars/wars.html", title="Wars", wars=wars)
+
+
+@bp.route("/wars/<war_id>")
+@login_required
+def war(war_id):
+    war = War.query.filter_by(id=war_id).first_or_404()
+    return render_template("wars/war.html", war=war)
