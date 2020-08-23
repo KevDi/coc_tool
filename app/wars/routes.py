d090a64110cf18f27d52888309a1fd4483c240f3
KevDi <kevin.dick91@yahoo.de>
2020-08-23 21:35:32 +0200

    changed ordering of wars

--- app/wars/routes.py
+++ app/wars/routes.py
@@ -7,7 +7,7 @@ from app.models import War
 @bp.route("/wars")
 @login_required
 def wars():
-    wars = War.query.all()
+    wars = War.query.order_by(War.start_time.desc()).all()
     return render_template("wars/wars.html", title="Wars", wars=wars)
 
 
